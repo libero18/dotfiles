9af904cccf619e40436a0d0f6861fcbe5fce172c
NAKAJIMA, Ryuichi (a.k.a. libero18) <orebil.18@gmail.com>
2015-06-24 16:42:14 +0900

    シンボリックリンク関連の不具合を修正

--- macos/files/.bin/Symlinks.sh
+++ macos/files/.bin/Symlinks.sh
@@ -4,40 +4,40 @@ function __readlinkf {
   TARGET_FILE=$1
 
   while [ "$TARGET_FILE" != "" ]; do
-      cd `dirname $TARGET_FILE`
-      FILENAME=`basename $TARGET_FILE`
-      TARGET_FILE=`readlink $FILENAME`
+      cd $(dirname $TARGET_FILE)
+      FILENAME=$(basename $TARGET_FILE)
+      TARGET_FILE=$(readlink $FILENAME)
   done
 
-  echo `pwd -P`/$FILENAME
+  echo $(pwd -P)/$FILENAME
 }
 
 scriptdir=$(cd $(dirname $(__readlinkf ${BASH_SOURCE:-$0})); pwd)
 
-cd `find ${scriptdir}/../../ -type d -name "files"`
+cd $(find ${scriptdir}/../../ -type d -name "files") >/dev/null 2>&1
 
 for dotfile in .?*
 do
   if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
   then
-    ln -Fs "$PWD/$dotfile" $HOME
+    ln -fs "$PWD/$dotfile" $HOME >/dev/null 2>&1
 fi
 done
 
 ## .ssh
 if [ -d ${HOME}/.repos/bitbucket.org/libero18/.ssh ]; then
-  ln -Fs $HOME/.repos/bitbucket.org/libero18/.ssh $HOME/.ssh
+  ln -fs $HOME/.repos/bitbucket.org/libero18/.ssh $HOME >/dev/null 2>&1
 fi
 
 ## anyenv
 if [ -d ${HOME}/.repos/github.com/riywo/anyenv ]; then
-  ln -Fs $HOME/.repos/github.com/riywo/anyenv $HOME/.anyenv
+  ln -fs $HOME/.repos/github.com/riywo/anyenv $HOME/.anyenv >/dev/null 2>&1
 fi
 
 ## oh-my-zsh theme
 if [ ! -L ${HOME}/.repos/github.com/robbyrussell/oh-my-zsh/custom/libero18.zsh-theme ]; then
-  ln -s ${HOME}/.repos/github.com/libero18/dotfiles/macos/files/.etc/libero18.zsh-theme ${HOME}/repos/github.com/robbyrussell/oh-my-zsh/custom/
+  ln -fs ${HOME}/.repos/github.com/libero18/dotfiles/macos/files/.etc/libero18.zsh-theme ${HOME}/repos/github.com/robbyrussell/oh-my-zsh/custom/ >/dev/null 2>&1
 fi
 
-cd -
+cd - >/dev/null 2>&1
 exit 0
--- macos/files/.zlogin
+++ macos/files/.zlogin
@@ -2,8 +2,8 @@
 
 ## ログイン時のみ最初にバックエンドで実行する
 {
-  if [ -f /Users/nakajima/repos/github.com/libero18/dotfiles/macos/files/.bin/Symlinks.sh ]; then
-    /Users/nakajima/repos/github.com/libero18/dotfiles/macos/files/.bin/Symlinks.sh
+  if [ -f /Users/nakajima/.repos/github.com/libero18/dotfiles/macos/files/.bin/Symlinks.sh ]; then
+    /Users/nakajima/.repos/github.com/libero18/dotfiles/macos/files/.bin/Symlinks.sh
   fi
   ## Heroku Toolbelt
   if [ -d /usr/local/heroku/bin ]; then
