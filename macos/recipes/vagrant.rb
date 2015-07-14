2bfe467b109e6f9898e240fc153bdafe698a40d6
NAKAJIMA, Ryuichi (a.k.a. libero18) <orebil.18@gmail.com>
2015-07-14 19:24:45 +0900

    add vagrant plugin update

--- macos/recipes/vagrant.rb
+++ macos/recipes/vagrant.rb
@@ -1,4 +1,4 @@
-### Install Vagrant Plugin
+### Vagrant Plugin
 to_enable = node['vagrant']['to_enable'] ? node['vagrant']['to_enable'] : false
 if to_enable
   node['vagrant']['pulgins'].each do |plugin|
@@ -7,11 +7,18 @@ if to_enable
       only_if "test $(which vagrant)"
     end
   end
+  enable_update = node['vagrant']['enable_update'] ? node['vagrant']['enable_update'] : false
+  if enable_update
+    execute "update vagrant plugins" do
+      command "vagrant plugin update"
+      only_if "test $(which vagrant)"
+    end
+  end
 else
-  Logger.info('Execution skipped install vagrant plugins because of not true to_enable')
+  Logger.info('Execution skipped vagrant plugins because of not true to_enable')
 end
 
-### Install Vagrant Box
+### Vagrant Box
 to_enable = node['vagrant']['to_enable'] ? node['vagrant']['to_enable'] : false
 if to_enable
   node['vagrant']['boxes'].each do |box|
@@ -21,5 +28,5 @@ if to_enable
     end
   end
 else
-  Logger.info('Execution skipped install vagrant boxes because of not true to_enable')
+  Logger.info('Execution skipped vagrant boxes because of not true to_enable')
 end
