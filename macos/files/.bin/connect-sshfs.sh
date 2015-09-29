bd9c602137b3f508673077e9781fcbdbffe7d9a0
NAKAJIMA, Ryuichi (a.k.a. libero18) <orebil.18@gmail.com>
2015-09-29 11:01:53 +0900

    接続先に応じてマウントする対象を変更できるように対応した

--- macos/files/.bin/connect-sshfs.sh
+++ macos/files/.bin/connect-sshfs.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
 CURRENT_MNT_DIR=/Users/nakajima/.mntsshfs
-REMOTE_DIR=${REMOTE_DIR:-/home/nakajima}
+
+case "$2" in
+mini)
+  REMOTE_DIR=/Users/nakajima/.repos
+  ;;
+*)
+  REMOTE_DIR=${REMOTE_DIR:-/home/nakajima}
+  ;;
+esac
 
 mount_sshfs() {
   if [ ! -d ${CURRENT_MNT_DIR}/$1 ]; then
-    mkdir ${CURRENT_MNT_DIR}/$1
+    mkdir -p ${CURRENT_MNT_DIR}/$1
   fi
   sshfs $1:${REMOTE_DIR} ${CURRENT_MNT_DIR}/$1
 }
