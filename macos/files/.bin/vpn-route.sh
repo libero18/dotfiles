cb5aa0cd51ac1126da582e6f230214223c88527d
NAKAJIMA, Ryuichi (a.k.a. libero18) <orebil.18@gmail.com>
2016-03-29 14:43:13 +0900

    ルーティング先の追加

--- macos/files/.bin/vpn-route.sh
+++ macos/files/.bin/vpn-route.sh
@@ -3,11 +3,13 @@
 start() {
   sudo route add 192.168.41.0/24 192.168.12.1
   sudo route add 192.168.11.0/24 192.168.12.1
+  sudo route add 192.168.12.0/24 192.168.12.1
 }
 
 stop() {
   sudo route delete 192.168.41.0/24
   sudo route delete 192.168.11.0/24
+  sudo route delete 192.168.12.0/24
 }
 
 case "$1" in
