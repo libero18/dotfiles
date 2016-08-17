edff9389d49554f0114ee81b23e74990d26d9079
NAKAJIMA, Ryuichi (a.k.a. libero18) <orebil.18@gmail.com>
2016-08-17 14:53:30 +0900

    鍵長をより安全なものに指定

--- macos/files/.bin/KeyGen.sh
+++ macos/files/.bin/KeyGen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 KEY_TYPE='rsa'
+KEY_LENGTH='4096'
 KEY_DIR="${HOME}/.ssh"
 KEY_NAME="${KEY_DIR}/id_rsa"
 TIME=`date +"%Y%m%d%H%M%S"`
@@ -8,8 +9,9 @@ TIME=`date +"%Y%m%d%H%M%S"`
 # 鍵を作成する
 [ -d $KEY_DIR ] || mkdir $KEY_DIR && chmod 700 $KEY_DIR
 [ -d $KEY_DIR ] && \
-	echo "please input Mail Address" && read MailAdress && echo -e '\n' && \
-	echo "please input password" && read PASSWD && echo -e '\n' && \
-	ssh-keygen -t ${KEY_TYPE} -f ${KEY_NAME}`[ -s $MailAdress ] || echo ".${MailAdress}"`.${TIME} -C ''${MailAdress} -P ''${PASSWD}
+	echo "please input Mail Address" && read MailAdress && echo '\n' && \
+	echo "please input password" && read PASSWD && echo '\n' && \
+	ssh-keygen -t ${KEY_TYPE} -b ${KEY_LENGTH} -C ''${MailAdress} -P ''${PASSWD} \
+        -f ${KEY_NAME}`[ -s $MailAdress ] || echo ".${MailAdress}"`.${TIME}
 
 exit 0
